0675202800da0f2f69fac5962d9006a773d3c3e3
NKDG78 <Kim.Aloshyn@motorolasolutions.com>
2020-09-27 20:43:49 +0200

    Initial commit

--- PrintDatesRange/Program.cs
+++ PrintDatesRange/Program.cs
@@ -2,14 +2,257 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace PrintDatesRange
 {
+    /// <summary>
+    /// The Main Date class.
+    /// It contsains some of the basic methods to work with dates.
+    /// </summary>
+    /// <remarks>
+    /// This class can compare dates and check if years is a leap one.
+    /// </remarks>
+    public class Date
+    {
+        Dictionary<int, int> daysCountToMonthMapping = new Dictionary<int, int>()
+        {
+            {1, 31}, {2, 28}, {3, 31}, {4, 30}, {5, 31}, {6, 30},
+            {7, 31}, {8, 31}, {9, 30}, {10, 31}, {11, 30}, {12, 31}
+        };
+
+        private int _day, _month, _year;
+
+        public int Day
+        {
+            get
+            { return _day; }
+            set
+            {
+                if (IsLeapYear())
+                    daysCountToMonthMapping[2] = 29;
+
+                if ((Enumerable.Range(1, daysCountToMonthMapping[Month]).Contains(value)))
+                    _day = value;
+                else
+                    throw new WrongDateInputException("Provided date does not exist. Check if day is correct.");
+            }
+        }
+
+        public int Month
+        {
+            get
+            { return _month; }
+            set
+            {
+                if (Enumerable.Range(1, 12).Contains(value))
+                    _month = value;
+                else
+                    throw new WrongDateInputException("Provided date does not exist. Check if month is correct.");
+            }
+        }
+
+        public int Year
+        {
+            get
+            { return _year; }
+            set
+            {
+                if (value > 0)
+                    _year = value;
+                else
+                    throw new WrongDateInputException("Provided date does not exist. Check if year is correct.");
+            }
+        }
+
+        /// <summary>
+        /// Constructor checks if given date suits the defined format. 
+        /// Provided string is split into day, month and year, 
+        /// which are parsed to Int32 and passed to properties. 
+        /// </summary>
+        /// <param name="date">A string which is supposed to be a date and provided by user.</param>
+        /// <exception cref="WrongDateInputException">Thrown when parameter does not suits 
+        /// the defined format.</exception>
+        public Date(string date)
+        {
+            bool isRightFormat = Regex.IsMatch(date, @"^(\d{1,2}\.){2}\d{1,}$");
+
+            if (!isRightFormat)
+                throw new WrongDateInputException("Provided date does not suite 'dd.mm.year' format.");
+
+            string[] dateArray = date.Split('.');
+
+            this.Year = Int32.Parse(dateArray[2]);
+            this.Month = Int32.Parse(dateArray[1]);
+            this.Day = Int32.Parse(dateArray[0]);
+        }
+
+        /// <summary>
+        /// Checks if year is a leap one.
+        /// </summary>
+        /// <returns>
+        /// Returns the bool value telling if year is leap or not.
+        /// </returns>
+        public bool IsLeapYear()
+        {
+            if (this.Year % 4 == 0)
+            {
+                if (this.Year % 100 == 0)
+                {
+                    return (this.Year % 400 == 0);
+                }
+                else
+                    return true;
+            }
+            else
+                return false;
+        }
+
+
+        public bool IsSameYear(Date comparedDate)
+        {
+            return Year == comparedDate.Year;
+        }
+
+        public bool IsSameMonth(Date comparedDate)
+        {
+            return Month == comparedDate.Month;
+        }
+
+        public bool IsSameDay(Date comparedDate)
+        {
+            return Day == comparedDate.Day;
+        }
+
+        /// <summary>
+        /// Generates a string with date. The format (dd or dd.mm or dd.mm.yyyy)
+        /// depends on the parameters printYear and printMonth.
+        /// </summary>
+        /// <param name="printYear"> Parameter which tells if the string should contain the year.</param>
+        /// <param name="printMonth">Parameter which tells if the string should contain the month.</param>
+        /// <returns>The string with date.</returns>
+        public string GetDateToPrint(bool printYear = true, bool printMonth = true)
+        {
+            StringBuilder dateToPrint = new StringBuilder();
+            dateToPrint.AppendFormat("{0:00}", Day);
+
+            if (printMonth)
+                dateToPrint.AppendFormat(".{0:00}", Month);
+            if (printYear)
+                dateToPrint.AppendFormat(".{0}", Year);
+
+            return dateToPrint.ToString(); ;
+        }
+
+        /// <summary>
+        /// Operator 'less than' is overloaded.
+        /// It does the comparison of two provided dates using properties (day, month and year) of provided objects.
+        /// </summary>
+        /// <param name="firstDate"> A Date object used for the comparison.</param>
+        /// <param name="secondDate">A Date object used for the comparison.</param>
+        /// <returns>A bool which tells if statement 'first date is less than second date" is true</returns>
+        public static bool operator <(Date firstDate, Date secondDate)
+        {
+            if (firstDate.Year < secondDate.Year)
+                return true;
+            else if (firstDate.Year == secondDate.Year)
+            {
+                if (firstDate.Month < secondDate.Month)
+                    return true;
+                else if (firstDate.Month == secondDate.Month)
+                {
+                    return firstDate.Day < secondDate.Day;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        //Summary: Same as for overloaded operator 'less then'.
+        public static bool operator >(Date firstDate, Date secondDate)
+        {
+            if (firstDate.Year > secondDate.Year)
+                return true;
+            else if (firstDate.Year == secondDate.Year)
+            {
+                if (firstDate.Month > secondDate.Month)
+                    return true;
+                else if (firstDate.Month == secondDate.Month)
+                {
+                    return firstDate.Day > secondDate.Day;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    };
+
+    [Serializable]
+    public class WrongDateInputException : Exception
+    {
+        public WrongDateInputException() { }
+
+        public WrongDateInputException(string message)
+            : base(message) { }
+
+        public WrongDateInputException(string message, Exception inner)
+            : base(message, inner) { }
+
+    }
+
     class Program
     {
+        /// <summary>
+        /// Main method takes 2 arguments from user (the execution is stopped when they are less or more than 2).
+        /// Arguments are suposed to be a String type and represent two dates. Arguments are used to create 2 instances
+        /// of Date class which are later compared. The result of comparison affects the order of dwo days which are printed at the end of program.
+        /// </summary>
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Input is invalid. You need to provide two arguments");
+                Environment.Exit(0);
+            }
+
+            Date firstDate = null;
+            Date secondDate = null;
+            bool printYear = true, printMonth = true, printOneDateOnly = false;
+
+            try
+            {
+                firstDate = new Date(args[0]);
+                secondDate = new Date(args[1]);
+            }
+            catch (WrongDateInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+
+            /*In this place we are checking if year, month and day are the same. Later it affects
+             the format of first date printed (dd or dd.mm or dd.mm.yyyy)
+             */
+            if (firstDate.IsSameYear(secondDate))
+            {
+                printYear = false;
+                if (firstDate.IsSameMonth(secondDate))
+                {
+                    printMonth = false;
+                    printOneDateOnly = firstDate.IsSameDay(secondDate);
+                }
+            }
+
+            if (printOneDateOnly)
+                Console.WriteLine(firstDate.GetDateToPrint());
+            else if (firstDate < secondDate)
+                Console.WriteLine(firstDate.GetDateToPrint(printYear, printMonth) + " - " + secondDate.GetDateToPrint());
+            else
+                Console.WriteLine(secondDate.GetDateToPrint(printYear, printMonth) + " - " + firstDate.GetDateToPrint());
         }
     }
 }
---
+++ PrintDatesRangeTests/DateTests.cs
@@ -0,0 +1,273 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrintDatesRange;
+
+namespace PrintDatesRange.Tests
+{
+    [TestClass()]
+    public class DateTests
+    {
+        [TestMethod()]
+        public void IsLeapYearTest()
+        {
+            Date firstLeapYearDate = new Date("29.02.1604");
+            Date secondLeapYearDate = new Date("29.02.1600");
+            Date firstNotLeapYearDate = new Date("28.02.1605");
+            Date secondNotLeapYearDate = new Date("28.02.1700");
+
+            bool leapResult = firstLeapYearDate.IsLeapYear() && secondLeapYearDate.IsLeapYear();
+            bool notLeapResult = firstNotLeapYearDate.IsLeapYear() || secondNotLeapYearDate.IsLeapYear();
+            bool result = leapResult && !notLeapResult;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void IsSameYearTest()
+        {
+            Date firstDateTrue = new Date("11.11.1918");
+            Date secondDateTrue = new Date("08.01.1918");
+
+            Date firstDateFalse = new Date("11.11.1918");
+            Date secondDateFalse = new Date("11.11.1");
+
+            bool isSameYearTrue = firstDateTrue.IsSameYear(firstDateTrue);
+            bool isSameYearFalse = firstDateFalse.IsSameYear(secondDateFalse);
+            bool result = isSameYearTrue && !isSameYearFalse;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void IsSameMonthTest()
+        {
+            Date firstDateTrue = new Date("03.11.1918");
+            Date secondDateTrue = new Date("08.11.1919");
+
+            Date firstDateFalse = new Date("09.11.875");
+            Date secondDateFalse = new Date("09.01.875");
+
+            bool isSameMonthTrue = firstDateTrue.IsSameMonth(secondDateTrue);
+            bool isSameMonthFalse = firstDateFalse.IsSameMonth(secondDateFalse);
+            bool result = isSameMonthTrue && !isSameMonthFalse;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void IsSameDayTest()
+        {
+            Date firstDateTrue = new Date("31.12.2019");
+            Date secondDateTrue = new Date("31.12.2020");
+
+
+            Date firstDateFalse = new Date("30.12.2020");
+            Date secondDateFalse = new Date("31.12.2020");
+
+            bool isSameDayTrue = firstDateTrue.IsSameDay(firstDateTrue);
+            bool isSameDayFalse = firstDateFalse.IsSameDay(secondDateFalse);
+            bool result = isSameDayTrue && !isSameDayFalse;
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void GetFullDateToPrintTest()
+        {
+            Date date = new Date("24.09.2020");
+            string expected = "24.09.2020";
+
+            string actual = date.GetDateToPrint(true, true).ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetDateWithNoYearToPrintTest()
+        {
+            Date date = new Date("24.09.2020");
+            string expected = "24.09";
+
+            string actual = date.GetDateToPrint(false, true).ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetOnlyDayToPrintTest()
+        {
+            Date date = new Date("24.09.2020");
+            string expected = "24";
+
+            string actual = date.GetDateToPrint(false, false).ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NotDigitInputThrowsException()
+        {
+            string expectedException = "Provided date does not suite 'dd.mm.year' format.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("q1.a2.187b");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void SlashSplitFormatInputThrowsException()
+        {
+            string expectedException = "Provided date does not suite 'dd.mm.year' format.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("01/02/1873");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void ThreeDigitMonthInputThrowsException()
+        {
+            string expectedException = "Provided date does not suite 'dd.mm.year' format.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("01.012.1873");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void ThreeDigitDayInputThrowsException()
+        {
+            string expectedException = "Provided date does not suite 'dd.mm.year' format.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("001.12.1873");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void LongerStringInputThrowsException()
+        {
+            string expectedException = "Provided date does not suite 'dd.mm.year' format.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("02.03.1982.02");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+
+        [TestMethod()]
+        public void WrongYearInputThrowsException()
+        {
+            string expectedException = "Provided date does not exist. Check if year is correct.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("02.03.0");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void WrongMonthInputThrowsException()
+        {
+            string expectedException = "Provided date does not exist. Check if month is correct.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("02.13.1987");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void WrongDayInputThrowsException()
+        {
+            string expectedException = "Provided date does not exist. Check if day is correct.";
+            Exception actualException = null;
+
+            try
+            {
+                Date date = new Date("29.02.1605");
+            }
+            catch (Exception ex)
+            {
+                actualException = ex;
+            }
+
+            Assert.AreEqual(expectedException, actualException.Message);
+        }
+
+        [TestMethod()]
+        public void CorrectDateInstanceCreation()
+        {
+            Exception exception = null;
+
+            try
+            {
+                Date date = new Date("24.02.1999");
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNull(exception);
+        }
+    }
+}
