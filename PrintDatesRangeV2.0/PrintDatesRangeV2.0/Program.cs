58cdd0c3499784176c5279f0ee20c3a2f2d2d847
NKDG78 <Kim.Aloshyn@motorolasolutions.com>
2020-10-08 17:42:07 +0200

    Second program added

---
+++ PrintDatesRangeV2.0/PrintDatesRangeV2.0/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PrintDatesRange2._0
+{
+    public class CustomizedDate
+    {
+        public DateTime Date { get; set; }
+
+        public CustomizedDate(string dateInput)
+        {
+            if (DateTime.TryParse(dateInput, out DateTime _temp))
+                Date = _temp;
+            else
+            {
+                Console.WriteLine("The date {0} is invalid.", dateInput);
+                Environment.Exit(1);
+            }
+        }
+
+        public string GetDateToPrint(bool getYear = true, bool getMonth = true)
+        {
+            if (!getYear)
+            {
+                if (!getMonth)
+                    return Date.ToString("dd");
+                else
+                    return Regex.Replace(Date.ToShortDateString(), @"[^\d]?" + Date.Year.ToString() + @"[^\d]?", "");
+            }
+            return Date.ToShortDateString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Input is invalid. You need to provide two arguments.");
+                Environment.Exit(1);
+            }
+
+            CustomizedDate firstDate = new CustomizedDate(args[0]);
+            CustomizedDate secondDate = new CustomizedDate(args[1]);
+
+            var (printYear, printMonth, printOneDateOnly) = (true, true, false);
+
+            if (firstDate.Date.Year == secondDate.Date.Year)
+            {
+                printYear = false;
+                if (firstDate.Date.Month == secondDate.Date.Month)
+                {
+                    printMonth = false;
+                    printOneDateOnly = (firstDate.Date.Day == secondDate.Date.Day);
+                }
+            }
+
+            if (printOneDateOnly)
+                Console.WriteLine(firstDate.GetDateToPrint());
+            else if (firstDate.Date < secondDate.Date)
+                Console.WriteLine(firstDate.GetDateToPrint(printYear, printMonth) + " - " + secondDate.GetDateToPrint());
+            else
+                Console.WriteLine(secondDate.GetDateToPrint(printYear, printMonth) + " - " + firstDate.GetDateToPrint());
+        }
+    }
+}
---
+++ PrintDatesRangeV2.0/PrintDatesRangeV2.0Tests/CustomizedDateTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrintDatesRange2._0;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintDatesRange2._0.Tests
+{
+    [TestClass()]
+    public class CustomizedDateTests
+    {
+        [TestMethod()]
+        public void GetFullDatePL()
+        {
+            var culture = CultureInfo.GetCultureInfo("pl-PL");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("28.12.1209");
+
+            string resultDate = date.GetDateToPrint();
+
+            Assert.AreEqual("28.12.1209", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetFullDateUS()
+        {
+            var culture = CultureInfo.GetCultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("5/30/1209");
+
+            string resultDate = date.GetDateToPrint();
+
+            Assert.AreEqual("5/30/1209", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetFullDateSE()
+        {
+            var culture = CultureInfo.GetCultureInfo("se-SE");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("1209-03-24");
+
+            string resultDate = date.GetDateToPrint();
+
+            Assert.AreEqual("1209-03-24", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetDateWithNoYearPL()
+        {
+            var culture = CultureInfo.GetCultureInfo("pl-PL");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("28.12.1209");
+
+            string resultDate = date.GetDateToPrint(false, true);
+
+            Assert.AreEqual("28.12", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetDateWithNoYearSE()
+        {
+            var culture = CultureInfo.GetCultureInfo("se-SE");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("1209-03-24");
+
+            string resultDate = date.GetDateToPrint(false, true);
+
+            Assert.AreEqual("03-24", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetDayOnlyPL()
+        {
+            var culture = CultureInfo.GetCultureInfo("pl-PL");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("28.12.1209");
+
+            string resultDate = date.GetDateToPrint(false, false);
+
+            Assert.AreEqual("28", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetDayOnlyUS()
+        {
+            var culture = CultureInfo.GetCultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("5/30/1209");
+
+            string resultDate = date.GetDateToPrint(false, false);
+
+            Assert.AreEqual("30", resultDate);
+        }
+
+        [TestMethod()]
+        public void GetDayOnlySE()
+        {
+            var culture = CultureInfo.GetCultureInfo("se-SE");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CustomizedDate date = new CustomizedDate("1209-03-24");
+
+            string resultDate = date.GetDateToPrint(false, false);
+
+            Assert.AreEqual("24", resultDate);
+        }
+    }
+}
